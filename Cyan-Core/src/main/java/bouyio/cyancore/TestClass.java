d15950449548305afe9309fccfc21ca39f409159
bouyio <bougiouklisgiorgos@gmail.com>
2025-03-18 20:56:06 +0200

    Made a little mistake. Oops...

---
+++ Cyan-Core/src/main/java/bouyio/cyancore/TestClass.java
@@ -0,0 +1,11 @@
+package bouyio.cyancore;
+
+/**
+ * This class is meant to be a test for build and version control systems.
+ * DO NOT USE.
+ * */
+@Deprecated
+public final class TestClass {
+    /** Do not attempt to instantiate it either.*/
+    private TestClass() {}
+}
