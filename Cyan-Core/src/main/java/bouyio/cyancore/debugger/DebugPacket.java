fc2f4b3c259ea882d511a4898bb5bd0b011547e4
bouyio <bougiouklisgiorgos@gmail.com>
2025-05-09 21:03:18 +0300

    Made docs for DebugPacket and StringIdentifiable.

--- Cyan-Core/src/main/java/bouyio/cyancore/debugger/DebugPacket.java
+++ Cyan-Core/src/main/java/bouyio/cyancore/debugger/DebugPacket.java
@@ -2,19 +2,31 @@ package bouyio.cyancore.debugger;
 
 import bouyio.cyancore.debugger.formating.StringIdentifiable;
 
+/**
+ * <p>Packets holding organised information that can be recorded using a Logger.<p/>
+ * <p>Where {@link H} is the type of the header of the packet that has a string identifier attached.<p/>
+ * <p>Where {@link V} is the type of the value of the packet.<p/>
+ * @see Logger
+ * @see StringIdentifiable
+ * */
 public class DebugPacket<H extends StringIdentifiable, V> {
     final H header;
     final V value;
 
+    /**
+     * <p>Creates a packet with specified header and value.<p/>
+     * */
     public DebugPacket(H header, V value) {
         this.header = header;
         this.value = value;
     }
 
+    /**<p>Returns the header of the packet.<p/>*/
     public H getHeader() {
         return header;
     }
 
+    /**<p>Returns the value of the packet.<p/>*/
     public V getValue() {
         return value;
     }
--- Cyan-Core/src/main/java/bouyio/cyancore/debugger/formating/StringIdentifiable.java
+++ Cyan-Core/src/main/java/bouyio/cyancore/debugger/formating/StringIdentifiable.java
@@ -1,5 +1,14 @@
 package bouyio.cyancore.debugger.formating;
 
+/**
+ * <p>
+ *    Allows for use of any object as a header for a packet.
+ * <p/>
+ *
+ * @see bouyio.cyancore.debugger.DebugPacket
+ * */
 public interface StringIdentifiable {
+
+    /**<p>Returns the identifier.<p/>*/
     String getIdentifier();
 }
