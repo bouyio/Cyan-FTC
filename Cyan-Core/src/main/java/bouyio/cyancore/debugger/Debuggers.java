7b64939111d188019f3d25b579b03c8b3f867fe0
bouyio <bougiouklisgiorgos@gmail.com>
2025-05-14 15:09:05 +0300

    Finished the docs for the debug package.

--- Cyan-Core/src/main/java/bouyio/cyancore/debugger/Debuggers.java
+++ Cyan-Core/src/main/java/bouyio/cyancore/debugger/Debuggers.java
@@ -1,14 +1,21 @@
 package bouyio.cyancore.debugger;
 
+/**
+ * <p>Contains globally accessible default instances of the all debugging tools.<p/>
+ * @see Logger
+ * */
 public class Debuggers {
 
-    private static Logger globalLogger;
+    private static Logger globalLogger = null;
 
+    /**<p>Instantiates and configures the debugging tools.<p/>*/
     public static void init() {
         globalLogger = new Logger();
     }
 
+    /**@return The default logger.*/
     public static Logger getGlobalLogger() {
+        if (globalLogger == null) init();
         return globalLogger;
     }
 }
