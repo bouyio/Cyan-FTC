fc0531d2fa16b22b96ebfabf7bdd4673f91a6f1d
bouyio <bougiouklisgiorgos@gmail.com>
2025-05-15 16:00:53 +0300

    Finished the docs for the geometry package.

--- Cyan-Core/src/main/java/bouyio/cyancore/geomery/Point.java
+++ Cyan-Core/src/main/java/bouyio/cyancore/geomery/Point.java
@@ -4,23 +4,43 @@ import java.util.Locale;
 
 import bouyio.cyancore.util.MathUtil;
 
+/**
+ * <p>Represents a point in a two-dimensional plain.<p/>
+ * @see Pose2D
+ * */
 public class Point {
     private final Pose2D coordinates;
     private final double distance;
 
-    public double getDistanceFromOrigin() {
-        return distance;
-    }
-
-    public Pose2D getCoordinates() {
-        return coordinates;
-    }
-
+    /**
+     * <p>Creates a point with specified coordinates.<p/>
+     * @param x The x coordinates of the point.
+     * @param y The y coordinates of the point.
+     * */
     public Point(double x, double y) {
         this.coordinates = new Pose2D(x, y, 0);
         this.distance = Math.sqrt(Math.pow(coordinates.getX(), 2) + Math.pow(coordinates.getY(), 2));
     }
 
+    /**
+     * @return The distance from the origin of the two coordinate axes (0,0).
+     * */
+    public double getDistanceFromOrigin() {
+        return distance;
+    }
+
+    /**
+     * @return The x and y coordinates formatted as {@link Pose2D}.
+     * */
+    public Pose2D getCoordinates() {
+        return coordinates;
+    }
+
+
+    /**
+     * <p>Formats the point's coordinates in a form easier for debugging.<p/>
+     * @return Formatted coordinates.
+     * */
     @Override
     public String toString() {
         return String.format(Locale.getDefault(),
@@ -30,24 +50,43 @@ public class Point {
                 coordinates.getTheta());
     }
 
+    /**
+     * <p>Calculates the hypotenuse of the difference of the coordinates between this and the given point.<p/>
+     * @param point The given point.
+     * @return The distance from the given point.
+     * */
     public double getDistanceFrom(Point point) {
         return MathUtil.hypotenuse(
                 coordinates.getX() - point.coordinates.getX(),
                 coordinates.getY() - point.coordinates.getY());
     }
 
+    /**
+     * <p>Calculates the hypotenuse of the difference of the coordinates between this point and the given pose.<p/>
+     * @param pose The given pose.
+     * @return The distance from the given pose.
+     * */
     public double getDistanceFrom(Pose2D pose) {
         return MathUtil.hypotenuse(
                 coordinates.getX() - pose.getX(),
                 coordinates.getY() - pose.getY());
     }
 
+    /**
+     * <p>Calculates the hypotenuse of the difference between this point's coordinates and the given coordinates.<p/>
+     * @param x The x of the given coordinates.
+     * @param y The y of the given coordinates.
+     * @return The distance from the given coordinates.
+     * */
     public double getDistanceFrom(double x, double y) {
         return MathUtil.hypotenuse(
                 coordinates.getX() - x,
                 coordinates.getY() - y);
     }
 
+    /**
+     * <p>Calculates and returns the angle of the polar coordinates of the point in Radians.<p/>
+     * */
     public double pointAngle() {
         return Math.atan2(coordinates.getY(), coordinates.getX());
     }
--- Cyan-Core/src/main/java/bouyio/cyancore/geomery/Pose2D.java
+++ Cyan-Core/src/main/java/bouyio/cyancore/geomery/Pose2D.java
@@ -1,28 +1,44 @@
 package bouyio.cyancore.geomery;
 
+/**
+ * <p>A container for 2D coordinate and heading info.<p/>
+ * */
 public class Pose2D {
     private final double x;
     private final double y;
     private final double theta;
 
+    /**
+     * <p>Creates a Pose object with specified coordinates and heading.<p/>
+     * @param x The x coordinates of the pose.
+     * @param y The y coordinates of the pose.
+     * @param theta The heading of the pose.
+     * */
     public Pose2D(double x, double y, double theta) {
         this.x = x;
         this.y = y;
         this.theta = theta;
     }
 
+    /**@return The angle of the pose.*/
     public double getTheta() {
         return theta;
     }
 
+    /**@return The coordinates of the pose in the x axis.*/
     public double getX() {
         return x;
     }
 
+    /**@return The coordinates of the pose in the y axis.*/
     public double getY() {
         return y;
     }
 
+    /**
+     * <p>Formats the x and y coordinates of the pose as a point.<p/>
+     * @return The coordinates of the pose as a point.
+     * */
     public Point toPoint() {
         return new Point(x, y);
     }
