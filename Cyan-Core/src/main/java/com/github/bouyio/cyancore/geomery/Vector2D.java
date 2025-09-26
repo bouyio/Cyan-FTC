52aedd06f2cb733b77dd5ac349206995496671bd
Bouyio <124091119+bouyio@users.noreply.github.com>
2025-09-26 19:27:57 +0300

    Added documentation for the Vector2D class.

--- Cyan-Core/src/main/java/com/github/bouyio/cyancore/geomery/Vector2D.java
+++ Cyan-Core/src/main/java/com/github/bouyio/cyancore/geomery/Vector2D.java
@@ -1,5 +1,8 @@
 package com.github.bouyio.cyancore.geomery;
 
+/**
+ * <p>This class represents a two-dimensional vector.</p>
+ * */
 public class Vector2D {
     private final double x;
     private final double y;
@@ -7,6 +10,11 @@ public class Vector2D {
     private final double r;
     private final double theta;
 
+    /**
+     * <p>Creates a vector with specified x and y values.<p/>
+     * @param x The x value of the point.
+     * @param y The y value of the point.
+     * */
     public Vector2D(double x, double y) {
 
         if (!Double.isFinite(x) || !Double.isFinite(y)) {
@@ -24,18 +32,22 @@ public class Vector2D {
         theta = Math.atan2(x, y);
     }
 
+    /**@return The cartesian x component of the vector.*/
     public double getCartesianX() {
         return x;
     }
 
+    /**@return The cartesian y component of the vector.*/
     public double getCartesianY() {
         return y;
     }
 
+    /**@return The radial r component of the vector.*/
     public double getRadialR() {
         return r;
     }
 
+    /**@return The radial theta component of the vector.*/
     public double getRadialTheta() {
         return theta;
     }
