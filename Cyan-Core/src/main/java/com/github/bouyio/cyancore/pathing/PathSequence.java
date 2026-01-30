c87c99d36ff44be758cd48598f1854181e2aa4ec
Bouyio <124091119+bouyio@users.noreply.github.com>
2026-01-30 21:09:33 +0200

    Added documentation for PathSequence

--- Cyan-Core/src/main/java/com/github/bouyio/cyancore/pathing/PathSequence.java
+++ Cyan-Core/src/main/java/com/github/bouyio/cyancore/pathing/PathSequence.java
@@ -1,10 +1,16 @@
 package com.github.bouyio.cyancore.pathing;
 
 import com.github.bouyio.cyancore.localization.PositionProvider;
+import com.github.bouyio.cyancore.pathing.engine.PathFollower;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <p>A simple way to sequence paths.<p/>
+ * @see Path
+ * @see PathFollower
+ * */
 public class PathSequence {
     List<Path> paths = new ArrayList<>();
 
@@ -13,6 +19,15 @@ public class PathSequence {
     private final double errorThreshold;
     private final PositionProvider positionProvider;
 
+    /**
+     * <p>
+     *     Creates a sequence with the provided paths with the given order and manually set path
+     *     error tolerance.
+     * <p/>
+     * @param posProvider The {@code PositionProvider} of the robot used for determining whether a path is finished.
+     * @param errorThreshold The error tolerance of the paths used for determining whether a path is finished.
+     * @param sequencePaths The paths to be sequenced in the given order.
+     * */
     public PathSequence(PositionProvider posProvider, double errorThreshold, Path... sequencePaths) {
         this.errorThreshold = errorThreshold;
         positionProvider = posProvider;
@@ -23,8 +38,27 @@ public class PathSequence {
         }
     }
 
+    /** @return The currently focused path of the sequence.*/
     private Path getCurrentPath() { return paths.get(currentPathIndex); }
 
+    /**
+     * <p>
+     *     Updates the sequence and returns the currently focused path.
+     * </p>
+     *
+     * <ul>
+     *   <li>
+     *       If the currently focused path is finished, the robot's distance to its last point is less
+     *       than the specified error threshold, it selects the next path.
+     *   </il>
+     *   <li>
+     *       If the sequence is finished, it returns null.
+     *   </il>
+     * </ul>
+     *
+     * @return The currently selected path or null if the sequence is finished.
+     * @implNote Calls {@link PositionProvider#update()}.
+     * */
     public Path nextPathUpdate() {
         if (currentPathIndex == paths.size() - 1) return null;
 
@@ -35,14 +69,32 @@ public class PathSequence {
         return getCurrentPath();
     }
 
+
+    /**
+     * <p>Adds a path to the end of the sequence.<p/>
+     * @param p The path to be added.
+     * */
     public void appendPath(Path p) {
         p.setMinimumPathError(errorThreshold);
         paths.add(p);
     }
 
-    public void insertPoint(int index, Path p) {
+    /**
+     * <p>Inserts a point at specific index of the sequence.<p/>
+     * @param index The index where the path is to be inserted.
+     * @param p The path to be inserted.
+     * */
+    public void insertPath(int index, Path p) {
         p.setMinimumPathError(errorThreshold);
         paths.add(index, p);
+    }
+
+
+    /**
+     * <p>Resets the sequence in order to be reused.<p/>
+     * */
+    public void reset() {
+        currentPathIndex = 0;
     }
 
 
